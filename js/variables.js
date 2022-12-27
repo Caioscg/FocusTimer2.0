33dec9bfb2d76510db9ec1ae65dbbc3e705de573
Caioscg <caio.gonzaga@discente.ufg.br>
2022-12-27 19:11:56 -0300

    first commit

---
+++ js/variables.js
@@ -0,0 +1,27 @@
+const playButton = document.querySelector(".play")
+const pauseButton = document.querySelector(".pause")
+const clockButton = document.querySelector(".set")
+const stopButton = document.querySelector(".stop")
+const plusButton = document.querySelector(".plus")
+const minusButton = document.querySelector(".minus")
+const treeCard = document.querySelector(".tree")
+const rainCard = document.querySelector(".rain")
+const houseCard = document.querySelector(".house")
+const fireCard = document.querySelector(".fire")
+let minutesDisplay = document.querySelector(".minutes")
+let secondsDisplay = document.querySelector(".seconds")
+
+export {
+    playButton,
+    pauseButton,
+    clockButton,
+    stopButton,
+    plusButton,
+    minusButton,
+    treeCard,
+    rainCard,
+    houseCard,
+    fireCard,
+    minutesDisplay,
+    secondsDisplay
+}
